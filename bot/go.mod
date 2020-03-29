973fb957b54e14f5a65f4651512beda66044c07e
gempir <daniel.pasch.s@gmail.com>
2020-03-29 10:40:07 +0200

    experiment with firehose

--- bot/go.mod
+++ bot/go.mod
@@ -3,7 +3,8 @@ module github.com/gempir/spamchamp
 go 1.13
 
 require (
-	github.com/gempir/go-twitch-irc v1.1.0
-	github.com/gempir/go-twitch-irc/v2 v2.2.2
-	github.com/gorilla/websocket v1.4.1
+	code.cerinuts.io/libs/goBlue v0.0.0-20200120201115-c0e31b8eeb08 // indirect
+	code.cerinuts.io/libs/goPurple v0.0.0-20200119200501-29bfa39fe36b
+	github.com/gorilla/websocket v1.4.2
+	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
 )
--- bot/main.go
+++ bot/main.go
@@ -1,36 +1,29 @@
 package main
 
 import (
-	"github.com/gempir/go-twitch-irc/v2"
+	"log"
+
+	"code.cerinuts.io/libs/goPurple/firehose"
 )
 
 var broadcast = make(chan socketMessage)
 
 type socketMessage struct {
-	Channel string       `json:"channel"`
-	Emote   twitch.Emote `json:"emote"`
-	Message string       `json:"message"`
+	Channel string `json:"channel"`
+	Message string `json:"message"`
 }
 
 func main() {
-	client := twitch.NewClient("justinfan123123", "oauth:123123123")
 
-	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
-		msg := &socketMessage{
-			Channel: message.Channel,
-			Message: message.Message,
-		}
+	firehose := &firehose.Firehose{}
+	msgQ, err := firehose.Connect("oauth")
+	if err != nil {
+		log.Fatal("firehose error: " + err.Error())
+	}
 
-		broadcast <- *msg
-
-		if len(message.Emotes) == 0 {
-			return
-		}
-	})
-
-	client.Join("gempir")
-
-	go client.Connect()
+	for event := range msgQ {
+		log.Println(event)
+	}
 
 	startWebsocketServer()
 }
